aecaaf3b47ad3eaaa0eade678de809f8af78475d
kedar <kedarv@users.noreply.github.com>
2013-12-08 11:29:55 -0600

    Added Background options

---
+++ sources/admin/background.php
@@ -0,0 +1,49 @@
+<script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/jscolor.js"></script>
+<?php 
+if($_SESSION['admin']){
+	if(!isset($_POST['url'])) {
+		echo "<h2 class=\"text-left\">Site Background</h2><hr/>
+		<p>Many sites have a background to make the website more personalized. There is not a default image size, but you may want to play around with some sizes to see what you like.</p>
+		<p>To upload an image, please go to <a href=\"http://www.imgur.com\">imgur.com</a>, and then enter in the image url below. The URL will look like this: i.imgur.com/abcdefghi.jpg. Of course, you may use any other website to host your image.</p><hr/>
+		<form method=\"post\">
+			<div class=\"form-group\">
+				<label for=\"inputURL\">Background URL</label>
+				<input type=\"text\" class=\"form-control\" name=\"url\" id=\"inputURL\" placeholder=\"Enter image URL\" value=\"".$background."\">
+			</div>
+			<div class=\"form-group\">
+				<label for=\"inputURL\">Background Color (Hex)</label>
+				<input type=\"text\" class=\"form-control color\" name=\"bgcolor\" id=\"inputURL\" placeholder=\"Enter Background Color\" value=\"".$bgcolor."\">
+				<span class=\"help-block\">Your background color must look like this: 000000<br/>To look up hex colors, click the input box above.</span>
+			</div>
+			<div class=\"form-group\">
+				<label for=\"repeatStyle\">Background Repeat</label>
+				<select class=\"form-control\" name=\"bgrepeat\" id=\"repeatStyle\">
+					<option value=\"no-repeat\">No Repeat</option>
+					<option value=\"repeat\">Repeat Both Directions</option>
+					<option value=\"repeat-x\">Repeat Horizontally</option>
+					<option value=\"repeat-y\">Repeat Vertically</option>
+				</select>
+				<span class=\"help-block\">Background images can repeat horizontally, vertically, both, or none.</span>
+			</div>
+			 <div class=\"checkbox\">
+				<label>
+					<input type=\"checkbox\" name=\"bgcenter\" value=\"center\">Center Background (Yes)
+				</label>
+			</div>	
+			<span class=\"help-block\">Background images can be centered.</span>
+			<hr/>
+			<button type=\"submit\" class=\"btn btn-primary\" required>Submit &raquo;</button>
+		</form>
+		";	
+	}
+	else {
+		$url = mysql_escape($_POST["url"]);
+		$bgcolor = mysql_escape($_POST["bgcolor"]);
+		$bgrepeat = mysql_escape($_POST["bgrepeat"]);
+		$bgcenter = mysql_escape($_POST["bgcenter"]);
+		$mysqli->query("UPDATE ".$prefix."properties SET background = '$url', bgcolor = '$bgcolor', bgrepeat = '$bgrepeat', bgcenter = '$bgcenter'");
+		echo "<div class=\"alert alert-success\">Successfully updated background.</div>";
+		redirect_wait5("?cype=admin&page=background");
+	}
+}
+?>
--- sources/structure/admin_header.php
+++ sources/structure/admin_header.php
@@ -5,8 +5,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo $sitetitle.$pb; ?></title>
 <link rel="icon" href="favicon.ico" type="image/x-icon" />
-<link href="assets/css/<?php echo $theme; ?>.min.css" rel="stylesheet" type="text/css" />
-<link href="assets/css/addon.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $siteurl; ?>assets/css/<?php echo $theme; ?>.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $siteurl; ?>assets/css/addon.css" rel="stylesheet" type="text/css" />
 <style type="text/css">
 body {
   min-height: 200px;
@@ -14,8 +14,8 @@ body {
 }
 </style>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-<script type="text/javascript" src="assets/js/bootstrap.js"></script>
-<script type="text/javascript" src="assets/js/login.js"></script>
+<script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/bootstrap.js"></script>
+<script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/login.js"></script>
 </head>
 
 <body>
