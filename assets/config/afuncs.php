a44a6bde44ff21563c618600d11841df7a14c0d0
Kedar <kedar54@gmail.com>
2013-12-21 18:18:44 -0600

    Added comment retouches

--- assets/config/afuncs.php
+++ assets/config/afuncs.php
@@ -683,4 +683,17 @@ function redirect_wait5($url) {
 	echo '<meta http-equiv="refresh" content="5;url='.$url.'" />';
 	exit;
 }
+
+function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+    $url = 'http://www.gravatar.com/avatar/';
+    $url .= md5( strtolower( trim( $email ) ) );
+    $url .= "?s=$s&d=identicon&r=$r";
+    if ( $img ) {
+        $url = '<img src="' . $url . '"';
+        foreach ( $atts as $key => $val )
+            $url .= ' ' . $key . '="' . $val . '"';
+        $url .= ' />';
+    }
+    return $url;
+}
 ?>
--- sources/structure/footer.php
+++ sources/structure/footer.php
@@ -3,10 +3,9 @@
 	</div></div>
 <footer>
 	<div class="container">
-		<p class="text-center">Proudly powered by Cype v0.1.0</p>
+		<p class="text-center">Proudly powered by LichtCMS v1.0.0-beta by <a href="http://forum.ragezone.com/members/1333360872.html">greenelf(x)</a></p><br/>
 	</div>
 </footer>
-
 </div>
 </body>
 </html>
