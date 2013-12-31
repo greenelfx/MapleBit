fa685e5746a30d64ec656673b7413a122fa1672c
kedar <kedarv@users.noreply.github.com>
2013-12-31 14:10:58 -0600

    Fixed various PHP errors

--- sources/ucp/charfix.php
+++ sources/ucp/charfix.php
@@ -3,35 +3,39 @@ if(isset($_SESSION['id'])){
 	if(@$_GET['fix'] == "unstuck"){
 		echo "<h2 class=\"text-left\">Move Character</h2><hr/>";
 		if(!isset($_POST['unstuck'])){
-			echo "
-		Is your character stuck at a bugged map, and everytime you login you get kicked back to your desktop?<br />
-		Fill out this form below, and your character will be warped to Henesys, and you should be able to log back in!
-		<hr/>
-			<form method=\"post\">
-				<b>Select character:</b><br/>
-			<select name=\"char\" class=\"form-control\">";
 			$s = $mysqli->query("SELECT * FROM characters WHERE accountid='".$_SESSION['id']."' ORDER BY id ASC") or die();
-			while($c = $s->fetch_assoc()){
+			$counts = $s->num_rows;
+			if($counts >= 1) {
 				echo "
-									<option value=\"".$c['id']."\">".$c['name']."</option>";
+				Is your character stuck at a bugged map, and everytime you login you get kicked back to your desktop?<br />
+				Fill out this form below, and your character will be warped to Henesys, and you should be able to log back in!
+				<hr/>
+				<form method=\"post\">
+					<b>Select character:</b><br/>
+				<select name=\"char\" class=\"form-control\">";
+				while($c = $s->fetch_assoc()){
+					echo "
+										<option value=\"".$c['id']."\">".$c['name']."</option>";
+				}
+				echo "
+				</select><br/>
+				<b>Spawn point:</b><br/>
+				<select name=\"map\" class=\"form-control\">
+					<option value=\"100000000\">Henesys</option>
+				</select><br/>
+				<input type=\"submit\" name=\"unstuck\" value=\"Unstuck &raquo;\" class=\"btn btn-info\"/>
+				</form><br/>";
+			} else {
+				echo "<div class=\"alert alert-danger\">You don't have any characters!";
 			}
-			echo "
-			</select><br/>
-			<b>Spawn point:</b><br/>
-			<select name=\"map\" class=\"form-control\">
-				<option value=\"100000000\">Henesys</option>
-			</select><br/>
-			<input type=\"submit\" name=\"unstuck\" value=\"Unstuck &raquo;\" class=\"btn btn-info\"/>
-			</form><br/>";
 		}else{
 			$char = $mysqli->real_escape_string($_POST['char']);
 			$henesys = $mysqli->real_escape_string($_POST['map']);
-			$dec = $mysqli->real_escape_string($_POST['dec']);
 			$m = $mysqli->query("UPDATE characters SET map='".$henesys."' WHERE id='".$char."'") or die();
 			echo "<div class=\"alert alert-success\"><b>Fix succesful.</b> Your character will now spawn at Henesys.</div>";
 			
 		}
-	}elseif(@$_GET['fix'] == "dc"){
+	} elseif(@$_GET['fix'] == "dc"){
 		echo "<h2 class=\"text-left\">Disconnect your Account</h2><hr/>";
 			if(!isset($_POST['dc'])){
 				echo "
