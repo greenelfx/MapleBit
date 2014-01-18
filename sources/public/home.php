c005e1e1acbf8648af1acef9dea71a20bf8bff9d
greenelfx <7076163+greenelfx@users.noreply.github.com>
2014-01-18 15:51:29 -0600

    double click to inline edit, fixed embed

--- sources/public/home.php
+++ sources/public/home.php
@@ -6,30 +6,38 @@ if(isset($_SESSION['id'])){
 <script src="assets/libs/ckeditor/ckeditor.js"></script>
 <script>
 $(document).ready(function() {
-    $("div[contenteditable='true']" ).each(function( index ) {
-        var content_id = $(this).attr('id');
-        CKEDITOR.inline( content_id, {
-            on: {
-                blur: function( event ) {
-                    var data = event.editor.getData();
-					$.ajax({
-                        type: "POST",
-						url: "?base=misc&script=home",
-                        data: {
-                            content : data,
-                            content_id : content_id,
-							admin_id : <?php echo $_SESSION['admin']; ?>,
-							is_ajax: '1',
-                        },
-                    });
-                }
-            }
-        });
-    });
+	$('#home').click(function() {
+		var name;
+		for(name in CKEDITOR.instances) {
+			var instance = CKEDITOR.instances[name];
+			if(this && this == instance.element.$) {
+				return;
+			}
+		}
+		$(this).attr('contenteditable', true);
+		var content_id = $(this).attr('id');
+			CKEDITOR.inline( content_id, {
+				on: {
+					blur: function( event ) {
+						var data = event.editor.getData();
+						$.ajax({
+							type: "POST",
+							url: "?base=misc&script=home",
+							data: {
+								content : data,
+								content_id : content_id,
+								admin_id : <?php echo $_SESSION['admin']; ?>,
+								is_ajax: '1',
+							},
+						});
+					}
+				}
+			});
+	});
 });
 </script>
 <?php
-		$editable = " contenteditable=\"true\" id=\"home\">";
+		$editable = " id=\"home\">";
 	}
 }
 echo "<hr/>
