dc6c900134392798c03a1a5c5317ae6ae2707262
greenelfx <7076163+greenelfx@users.noreply.github.com>
2014-02-12 17:08:59 -0600

    new css

--- assets/config/properties.php
+++ assets/config/properties.php
@@ -47,7 +47,7 @@ $gmlevel = $prop['gmlevel'];
 $MaxAcc = $prop['maxaccounts'];
 /* Get Theme */
 $theme = $prop['theme'];
-$getdarkhemes = array("cyborg", "slate", "amelia");
+$getdarkhemes = array("cyborg", "slate", "amelia", "superhero");
 if (in_array($theme, $getdarkhemes)) {
     $themetype = "dark";
 } else{
