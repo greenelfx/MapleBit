fe1fddc5cafff7516432fbc6ae7db9aba3693706
Kedar <kedar54@gmail.com>
2014-06-02 15:33:42 -0500

    Login form can use enter button

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,28 +1,30 @@
-$(function() {
+$(function () {
     var action = '';
     var form_data = '';
     $('#login').click(function () {
-         action = $("#loginform").attr("action");
-         form_data = {
-         username: $("#username").val(),
-         password: $("#password").val(),
-         is_ajax: '1'};  
+        event.preventDefault();
+        action = $("#loginform").attr("action");
+        form_data = {
+            username: $("#username").val(),
+            password: $("#password").val(),
+            is_ajax: '1'
+        };
 
         $.ajax({
             type: 'POST',
-            url: action,
-            data: form_data, 
-            success: function(response) {
-                 if(response == 'success') {
-                   $("#loginform").slideUp('slow', function() {
-                     $("#message").html('<script>location.reload();</script><div class=\"alert alert-success\">Logged in. Reloading...</div>');
-                   });
-                 } else { 
+            url: '?base=misc&script=login',
+            data: form_data,
+            success: function (response) {
+                if (response == 'success') {
+                    $("#loginform").slideUp('slow', function () {
+                        $("#message").html('<script>location.reload();</script><div class=\"alert alert-success\">Logged in. Reloading...</div>');
+                    });
+                } else {
                     $('#message').hide().html("<br/><div class=\"alert alert-danger\">Wrong username or password</div>").fadeIn('fast');
-                 }
-				 console.log(response);
+                }
+                console.log(response);
             }
-         }); 
-        return false;      
-    });  
+        });
+        return false;
+    });
 });
