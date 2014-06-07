2437db7637c9b2730ef977bfc9032b1e4b9c4b90
Kedar <kedar54@gmail.com>
2014-06-07 07:52:16 -0500

    Temporary GD changes, fixed login

---
+++ assets/img/GD/create.php
@@ -0,0 +1,153 @@
+<?php
+error_reporting(0);
+/*
+	+--------------------------------------------------------+
+	|                   Made by HoltHelper                   |
+	|			 Edited by LittleClgt@RaGEZONE.Com			 |
+	|                                                        |
+	| Please give proper credits when using this code since  |
+	| It took me over a couple of months to finish this code |
+	|                                                        |
+	+--------------------------------------------------------+
+	
+	Added Ian edits
+*/
+require_once('../../config/database.php');
+require_once('coordinates.php');
+if(!empty($_GET['name'])) {
+	$name = preg_replace("/[^a-zA-Z0-9]+/", "", $_GET['name']);
+	$Image  = new Character;
+	$cache = "Characters/".$name.".png";
+
+	if(file_exists($cache) && (time() - (43200) < filemtime($cache))) {
+		$Image->charType('use', $name);
+	} else {
+		if($gotChar = mysqli_fetch_row($mysqli->query("SELECT `id`, `skincolor`, `gender`, `hair`, `face` FROM `characters` WHERE `name` = '$name' LIMIT 1"))) {
+			$getAcc = $mysqli->query("SELECT `itemid`, `position` FROM `inventoryitems` WHERE `characterid` = '{$gotChar[0]}' AND `inventorytype` = '-1' ORDER BY `position` DESC");
+			$oHash  = mysqli_fetch_row($mysqli->query("SELECT `hash` FROM `bit_gdcache` WHERE `id` = '{$gotChar[0]}' LIMIT 1"));
+			$cap    = $mask = $eyes = $ears = $coat = $pants = $shoes = $glove = $cape = $shield = $weapon = NULL; // ANTI rapage
+
+			while($gotAcc = mysqli_fetch_row($getAcc)) {
+				switch($gotAcc[1]) {
+					case -1: case -101:$cap    = $gotAcc[0];break;
+					case -2: case -102:$mask   = $gotAcc[0];break;
+					case -3: case -103:$eyes   = $gotAcc[0];break;
+					case -4: case -104:$ears   = $gotAcc[0];break;
+					case -5: case -105:$coat   = $gotAcc[0];break;
+					case -6: case -106:$pants  = $gotAcc[0];break;
+					case -7: case -107:$shoes  = $gotAcc[0];break;
+					case -8: case -108:$glove  = $gotAcc[0];break;
+					case -9: case -109:$cape   = $gotAcc[0];break;
+					case -10:case -110:$shield = $gotAcc[0];break;
+					case -11:case -111:
+						$weapon = $gotAcc[0];
+						$Image->setWepInfo($weapon);
+					break;
+				}
+			}
+			if ($weapon == null){
+				$weapon = 1;
+				$Image->setWepInfo($weapon);
+			}
+			$nHash = hash("sha1", $cap.$mask.$eyes.$ears.$coat.$pants.$shoes.$glove.$cape.$shield.$weapon);
+			
+			if($nHash === $oHash[0]) {
+				$Image->charType('use', $name);
+				touch($cache);
+			} else {
+				$Image->setVaribles(array(
+					"Skin"   => $gotChar[1],
+					"Gender" => $gotChar[2],
+					"Hair"   => $gotChar[3],
+					"Face"   => $gotChar[4],
+					"Cap"    => $cap,
+					"Mask"   => $mask,
+					"Eyes"   => $eyes,
+					"Ears"   => $ears,
+					"Coat"   => $coat,
+					"Pants"  => $pants,
+					"Shoes"  => $shoes,
+					"Glove"  => $glove,
+					"Cape"   => $cape,
+					"Shield" => $shield,
+					"Weapon" => $weapon
+				));
+				$Image->setWeapon('weaponBelowBody');
+				$Image->setCap('capeBelowBody');
+				$Image->setCap('capBelowHead');
+				$Image->setCap('capAccessoryBelowBody');
+				$Image->setCape('cape');
+				$Image->setCape('backWing');
+				$Image->setCap('backCap');
+				$Image->setCape('capeBelowBody');
+				$Image->setCap('capeBelowBody');
+				$Image->setShield();
+				$Image->setHair('hairBelowBody');
+				$Image->setShoes('capAccessoryBelowBody');
+				$Image->setWeapon('weaponOverArmBelowHead');
+				$Image->createBody('body');
+				$Image->setShoes('shoes');
+				$Image->setShoes('weaponOverBody');
+				$Image->setGlove('l', 1);
+				$Image->setWeapon('weaponOverBody');
+				$Image->setPants();
+				$Image->setCoat('mail');
+				$Image->setShoes('shoesTop');
+				$Image->setShoes('shoesOverPants');
+				$Image->setShoes('pantsOverMailChest');
+				$Image->setShoes('gloveWristBelowMailArm');
+				$Image->setWeapon('armBelowHeadOverMailChest');
+				$Image->setHair('hairBelowHead');
+				$Image->setCap('capBelowHead');
+				$Image->createBody('head');
+				$Image->setAccessory('Ears', 'accessoryEar');
+				$Image->setCap('backHairOverCape');
+				$Image->setCap('backHair');
+				$Image->setHair('hairShade');
+				$Image->setCap('capAccessoryBelowAccFace');
+				$Image->setAccessory('Mask', 'accessoryFaceBelowFace');
+				$Image->setAccessory('Eyes', 'accessoryEyeBelowFace');
+				$Image->setFace();
+				$Image->setAccessory('Mask', 'accessoryFace');
+				$Image->setCap('accessoryEyeOverCap'); 
+				$Image->setAccessory('Eyes', 'accessoryEye');
+				$Image->setCap('accessoryEar');
+				$Image->setHair('hair');
+				$Image->setHair('hairOverHead');
+				$Image->setAccessory('Ears', 'accessoryEarOverHair');
+				$Image->setAccessory('Eyes', 'accessoryOverHair');
+				$Image->setAccessory('Eyes', 'hairOverHead');
+				$Image->setCap('capBelowAccessory');
+				$Image->setCap('0');
+				$Image->setCap('cap');
+				$Image->setCap('body');
+				$Image->setCap('capOverHair');
+				$Image->setAccessory('Mask', 'capOverHair');
+				$Image->setAccessory('Eyes', 'accessoryEyeOverCap');
+				$Image->setAccessory('Mask', 'capeOverHead');
+				$Image->setCape('capeOverHead');
+				$Image->setCape('capOverHair');
+				$Image->setWeapon('weapon');
+				$Image->createBody('arm');
+				$Image->setShield('weaponOverArmBelowHead');
+				$Image->setWeapon('weaponBelowArm');
+				$Image->setCoat('mailArm');
+				$Image->setCape('capeArm');
+				$Image->setWeapon('weaponOverArm');
+				$Image->createBody('hand');
+				$Image->setGlove('l', 2);
+				$Image->setGlove('r');
+				$Image->setWeapon('weaponOverHand');
+				$Image->setWeapon('weaponOverGlove');
+				$Image->setWeapon('weaponWristOverGlove');
+				$Image->setWeapon('emotionOverBody');
+				$Image->setWeapon('characterEnd');
+				
+				
+				$Image->charType('create', $name);
+			}
+		} else
+			$Image->charType('use', 'faek');
+	}
+}
+?>
--- sources/public/main-rank.php
+++ sources/public/main-rank.php
@@ -14,10 +14,6 @@ echo "
 <div class=\"col-md-6\">
 <a href='?base=main&amp;page=rankings'><h4>Rankings &raquo;</h4></a><hr/>";
 	$gc = $mysqli->query("SELECT c.$first , c.$second, c.name, c.accountid, a.banned AS banned FROM characters c LEFT JOIN accounts a ON c.accountid = a.id WHERE c.gm < '$gmlevel' AND banned = 0 GROUP BY c.id DESC ORDER BY $first DESC, $second DESC LIMIT 5");
-	$backcolor="";
-	$rootfolder = "";
-	require_once("assets/img/GD/coordinates.php");
-	require_once("assets/img/GD/cache_character.php");
 	echo "
 <table class=\"table table-condensed\">
 	<thead>
@@ -34,18 +30,16 @@ echo "
 	while($player = $gc->fetch_assoc() and $p <=5){
 		$char = $player['accountid'];
 		$name = $player['name'];
-		createChar($name, $rootfolder);
-		$cachechar = $mysqli->query("SELECT hash, name FROM ".$prefix."gdcache WHERE name='".$name."'")->fetch_assoc();
 		$p++;
 			if ($p == 1){
 				echo "
-				<img src=\"assets/img/GD/Characters/".$cachechar['hash'].".png\" alt='".$cachechar['name']."' name=\"top5\"/>
+				<img src=\"assets/img/GD/create.php?name=".$name."\" alt='".$name."' name=\"top5\"/>
 			</td>";
 			}
 				echo "
 				<tr>
 					<td>
-						<a href=\"?base=main&amp;page=character&amp;n=".$player['name']."\" onmouseover=\"roll('top5', 'assets/img/GD/Characters/".$cachechar['hash'].".png')\">".$player['name']."</a>
+						<a href=\"?base=main&amp;page=character&amp;n=".$name."\" onmouseover=\"roll('top5', 'assets/img/GD/Characters/".$name.".png')\">".$name."</a>
 					</td>
 					<td>".$player[$first]."</td>
 				</tr>
