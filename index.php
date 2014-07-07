447332f44e2c764a7ea6fed3b17c9d4fae7cc29c
Kedar <kedar54@gmail.com>
2014-07-07 16:43:00 -0500

    Removed debug, misc admin main changes

--- index.php
+++ index.php
@@ -13,15 +13,6 @@ if(!file_exists('assets/config/install/installdone.txt')){
 	# Import Essential Files
 	require_once("assets/config/properties.php");
 	require_once("assets/config/afuncs.php");
-	if($prop['debug'] == 1 && isset($_SESSION['admin'])){
-		require( 'assets/libs/php_error.php' );
-		\php_error\reportErrors();
-	}
-	if($prop['debug'] == 1  && !isset($_SESSION['admin'])){
-		include("sources/structure/maintenancedebug.php");
-		include("sources/structure/footer.php");
-		die();
-	}
 	# Define $getbase variable
 	$getbase = isset($_GET['base']) ? $_GET['base'] : "";
 
