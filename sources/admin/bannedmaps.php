62e33b8dc44ae26fb9bc43affd80734544e49fa9
Kedar <kedar54@gmail.com>
2015-05-01 15:38:14 -0400

    cleaned up banned_maps
    replaced implementation entirely
    PHP code is cleaner, and jQuery code is easier to maintain

--- sources/admin/bannedmaps.php
+++ sources/admin/bannedmaps.php
@@ -14,55 +14,59 @@ if($_SESSION['admin']){
 		Prevent users from warping out of jail from the User Control Panel.<br/><br/>
 		<form role=\"form\" method=\"post\">
 			<div class=\"form-group\">
-				<input type=\"hidden\" name=\"count\" value=\"".$totalids."\" id=\"count\"/>";
+				<div class=\"input_fields_wrap\">";
 				for($i = 0; $i <= $totalids-1; $i++) {
 					$n = $i+1;
-					echo "<input autocomplete=\"off\" class=\"form-control\" id=\"field".$n."\" name=\"field".$n."\" type=\"text\" placeholder=\"Jail Map ID\" value=\"".$mapid[$i]."\"><br/>";
+					echo "<div class=\"row\" style=\"margin-bottom:15px;\"><div class=\"col-md-10\"><input autocomplete=\"off\" class=\"form-control\" type=\"text\" placeholder=\"Jail Map ID\" type=\"text\" name=\"input_map[]\"  value=\"".$mapid[$i]."\"></div><div class=\"col-md-2\"><a href=\"#\" class=\"remove_field btn btn-danger\">Remove</a><br/></div></div>";
 				}
 		echo "	
-			</div>
+			</div></div></div>
 			<span class=\"input-group-btn\">
-				<button id=\"b1\" class=\"btn btn-info add\" type=\"button\">Add another Map</button>
+				<button id=\"b1\" class=\"btn btn-info add_field_button\" type=\"button\">Add another Map</button>
 			</span>
 			<hr/>
 			<button class=\"btn btn-primary\" name=\"submit\">Submit &raquo;</button>
-		<script>
-			$(document).ready(function(){
-			var next = ".$totalids.";
-			$(\".add\").click(function(e){
-				e.preventDefault();
-				var addto = \"#field\" + next;
-				next = next + 1;
-				var newIn = '<br/><input autocomplete=\"off\" class=\"form-control\" id=\"field' + next + '\" name=\"field' + next + '\" type=\"text\" placeholder=\"Jail Map ID\">';
-				var newInput = $(newIn);
-				$(addto).after(newInput);
-				$(\"#field\" + next).attr('data-source',$(addto).attr('data-source'));
-				$(\"#count\").val(next);  
-			});
-		});
-		</script>
 		";
 	} else {
-		$fields = $mysqli->real_escape_string(preg_replace("/[^A-Za-z0-9 ]/", '', $_POST['count'])); # Get # of fields
-		for($i = 1; $i <= $fields; $i++) {
-			$fieldval = $mysqli->real_escape_string(preg_replace("/[^A-Za-z0-9 ]/", '', $_POST['field'.$i.''])); # Get field value
+		$input_map = $_POST['input_map'];
+		$maps = array();
+		$i = 1;
+		foreach($input_map as $key => $n ) {
+			$fieldval = $mysqli->real_escape_string(preg_replace("/[^A-Za-z0-9 ]/", '', $n));
 			if(!is_numeric($fieldval)) {
-				echo "<div class=\"alert alert-danger\">Please make sure to only use numbers! Check map entry #".$i.".</div>";
-				$pass = false;
-			} else {
-				$maps[] = $fieldval;
-				$pass = true;
+				echo "<div class=\"alert alert-danger\">Please make sure to only use numbers! Check map entry #".$i.".</div><hr/><button onclick=\"goBack()\" class=\"btn btn-primary\">&laquo; Go Back</button>";
+				exit();
 			}
+			else {
+				$maps[] = $fieldval;
+			}
+			$i++;
 		}
-		if($pass) {
-			$maplist = implode(",", $maps);
-			$mysqli->query("UPDATE ".$prefix."properties SET jailmaps = '$maplist'");
-			echo "<div class=\"alert alert-success\">Successfully updated jail maps</div><hr/><a href=\"?base=admin\" class=\"btn btn-primary\">&laquo; Admin Panel</a>";
-		} else {
-			echo "<hr/><a href=\"?base=admin&page=bannedmaps\" class=\"btn btn-primary\">&laquo; Go Back</a>";
-		}
+
+		$maplist = implode(",", $maps);
+		$mysqli->query("UPDATE ".$prefix."properties SET jailmaps = '$maplist'");
+		echo "<div class=\"alert alert-success\">Successfully updated jail maps</div><hr/><a href=\"?base=admin\" class=\"btn btn-primary\">&laquo; Admin Panel</a>";
 	}
 } else{
 	redirect("?base");
 }
 ?>
+<script>
+$(document).ready(function() {
+    var max_fields      = 10;
+    var wrapper         = $(".input_fields_wrap");
+    var add_button      = $(".add_field_button");
+    var x = 1;
+    $(add_button).click(function(e){
+        e.preventDefault();
+        if(x < max_fields){
+            x++;
+            $(wrapper).append('<div class=\"row\" style=\"margin-bottom:15px;\"><div class="col-md-10"><input autocomplete="off" class="form-control" type="text" placeholder="Jail Map ID" type="text" name="input_map[]"/></div><div class="col-md-2"><a href="#" class="remove_field btn btn-danger">Remove</a></div></div>'); //add input box
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field", function(e){
+        e.preventDefault(); $(this).parent('div').parent('div').remove(); x--;
+    })
+});
+</script>
