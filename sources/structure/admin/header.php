22172cca38e45354ffb622ae96d6d287ea668f73
greenelfx <7076163+greenelfx@users.noreply.github.com>
2015-08-02 10:29:46 -0600

    upgrade to jquery 2.1.4, upgrade bootstrap js to 3.3.5

--- sources/structure/admin/header.php
+++ sources/structure/admin/header.php
@@ -28,8 +28,8 @@ body {
 }        
 .nav > li > a { color: #787878 }  
 </style>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/bootstrap.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/login.js"></script>
 <script type='text/javascript'>
 $(document).ready(function() {
