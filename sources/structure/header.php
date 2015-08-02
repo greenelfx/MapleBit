f5c53a73fe6a80f44d0b1a8689d344eb03b4b2c6
greenelfx <7076163+greenelfx@users.noreply.github.com>
2015-08-02 10:29:46 -0600

    upgrade to jquery 2.1.4, upgrade bootstrap js to 3.3.5

--- sources/structure/header.php
+++ sources/structure/header.php
@@ -28,8 +28,8 @@ body{
 	
 }
 </style>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/bootstrap.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/login.js"></script>
 <script>
 	function roll(img_name1, img_src1) {document[img_name1].src = img_src1;}
