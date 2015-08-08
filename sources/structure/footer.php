68fd89ebc92328a3a5097271a504d814cd931018
greenelfx <7076163+greenelfx@users.noreply.github.com>
2015-08-08 15:14:56 -0600

    move js to bottom, known issue: redundant js on home if admin

--- sources/structure/footer.php
+++ sources/structure/footer.php
@@ -4,13 +4,20 @@ if(basename($_SERVER["PHP_SELF"]) == "footer.php"){
 }
 ?>
 	</div>
-		<br/>
-	</div></div>
+<br/>
+</div></div>
 <footer>
 	<div class="container">
 		<p class="text-center">Proudly powered by MapleBit | <a href="http://forum.ragezone.com/members/1333360872.html">greenelf(x) &raquo;</a></p><br/>
 	</div>
 </footer>
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo $siteurl; ?>assets/js/login.js"></script>
+<script>
+	function roll(img_name1, img_src1) {document[img_name1].src = img_src1;}
+	function goBack() {window.history.back()}
+</script>
 </body>
 </html>
