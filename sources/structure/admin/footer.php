40f984330003451b0dca8a96b3161d11296ee39d
Kedar V <kedar54@gmail.com>
2015-08-10 20:12:36 -0600

    fix JS in admin panel

---
+++ sources/structure/admin/footer.php
@@ -0,0 +1,20 @@
+<?php
+if(basename($_SERVER["PHP_SELF"]) == "footer.php"){
+    die("403 - Access Forbidden");
+}
+?>
+	</div>
+<br/>
+</div></div>
+<footer>
+	<div class="container">
+		<p class="text-center">Proudly powered by MapleBit | <a href="http://forum.ragezone.com/members/1333360872.html">greenelf(x) &raquo;</a></p><br/>
+	</div>
+</footer>
+</div>
+<script>
+	function roll(img_name1, img_src1) {document[img_name1].src = img_src1;}
+	function goBack() {window.history.back()}
+</script>
+</body>
+</html>
