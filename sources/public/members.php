9a407fbed542256859655a7a680906f241c6f79a
Kedar <kedar54@gmail.com>
2016-03-07 15:54:31 -0500

    fix unset errors

--- sources/public/members.php
+++ sources/public/members.php
@@ -2,77 +2,75 @@
 if(basename($_SERVER["PHP_SELF"]) == "members.php"){
     die("403 - Access Forbidden");
 }
-$name = $_SESSION['name'];
-if(isset($_GET['name'])){$name = sql_sanitize($_GET['name']);}
-$check = $mysqli->query("SELECT * FROM `accounts` WHERE `id`='".getInfo('accid', $name, 'profilename')."'") or die();
-$real = $check->num_rows;
-if($real < 1){
-	if($_SESSION){
-		$name = $_SESSION['name'];
-	} else{
-		$name = "";
-		echo "<meta http-equiv=refresh content=\"0; url=?base=main\">";
-		exit();
+
+if(isset($_GET['name'])) {
+	$name = $mysqli->real_escape_string($_GET['name']);
+	$check = $mysqli->query("SELECT COUNT(*) FROM `accounts` WHERE `id`='".getInfo('accid', $name, 'profilename')."'")->fetch_row();
+	if($check[0] != 1){ // If the requested user doesn't exist, use the current user
+		if($_SESSION){
+			$name = $_SESSION['name'];
+		} else { // Else redirect to the homepage
+			echo "<meta http-equiv=refresh content=\"0; url=?base=main\">";
+			exit();
+		}
 	}
-}
 
-if(isset($_GET['name'])){
-		$ga = $mysqli->query("SELECT * FROM `accounts` WHERE `id`='".getInfo('accid', $name, 'profilename')."'") or die();
-		$a = $ga->fetch_assoc();
-		if($a['loggedin'] == "0"){
-			$status = "<span class=\"label label-danger\">Offline</span>";
-		}else{
-			$status = "<span class=\"label label-success\">Online</span>";
-		}
-		$gp = $mysqli->query("SELECT * FROM `".$prefix."profile` WHERE `name`='".$name."'") or die();
-		$p = $gp->fetch_assoc();
-		$mc = $p['mainchar'];
-		$gmc = $mysqli->query("SELECT * FROM `characters` WHERE `id`='".$mc."'") or die();
-		$m = $gmc->fetch_assoc();
-		require_once 'assets/libs/HTMLPurifier.standalone.php';
-		$config = HTMLPurifier_Config::createDefault();
-		$config->set('HTML.Allowed', 'p, b, u, s, ol, li, ul, i, em, strong');
-		$purifier = new HTMLPurifier($config);
-		$clean_html = $purifier->purify($p['text']);
-		if(empty($p['realname'])){
-			$p['realname'] = "";
-		} else {
-			$p['realname'] = "(" . $p['realname'] . ")";
-		}
-		echo "<h2 class=\"text-left\"><img src=\"".get_gravatar($a['email'])."\" class=\"img-circle\">&nbsp;".$name." ".htmlspecialchars($p['realname'], ENT_QUOTES, 'UTF-8')."</h2>
-		<hr/>
-		".$status." in game and ".onlineCheck(getInfo('accid', $name, 'profilename'))." on the site
-		<hr/>";
-		if(!$m['name'] == "") {
-			echo "<b>Main Character:</b> ".htmlspecialchars($m['name'], ENT_QUOTES, 'UTF-8'). "<br/>";
-		}
-		if(!empty($p['country'])) {
-			echo "<b>Country: </b>".htmlspecialchars($p['country'], ENT_QUOTES, 'UTF-8')."<br/>";
-		}
-		if(!empty($p['motto'])) {
-			echo "<b>Motto:</b> ".htmlspecialchars($p['motto'], ENT_QUOTES, 'UTF-8')."<br/>";
-		}
-		if(!empty($p['age'])) {
-			echo "<b>Age:</b> ".htmlspecialchars($p['age'], ENT_QUOTES, 'UTF-8')."<br/>";
-		}
-		if(!empty($p['favjob'])) {
-			echo "<b>Favorite Job: </b>".htmlspecialchars($p['favjob'], ENT_QUOTES, 'UTF-8')."<br/><br/>";
-		}
-		if(!empty($p['text'])) {
-			echo "
-				<b>About Me:</b>
-				<div class=\"breakword\">" . $clean_html."</div>
-				<hr/>";
-		}
-		if(isset($_SESSION['pname'])) {
-			echo "<a href=\"?base=ucp&amp;page=mail&amp;uc=$name\">Send me Mail &raquo;</a>";
-		}
-		if(isset($_SESSION['pname']) && $_GET['name'] == $_SESSION['pname']) {
-			echo "<hr/><a href=\"?base=ucp&page=profedit\">Edit Profile &raquo;</a><hr/>";
-		}
+	$ga = $mysqli->query("SELECT * FROM `accounts` WHERE `id`='".getInfo('accid', $name, 'profilename')."'") or die();
+	$a = $ga->fetch_assoc();
+	if($a['loggedin'] == "0") {
+		$status = "<span class=\"label label-danger\">Offline</span>";
+	} else{
+		$status = "<span class=\"label label-success\">Online</span>";
+	}
+	$gp = $mysqli->query("SELECT * FROM `".$prefix."profile` WHERE `name`='".$name."'") or die();
+	$p = $gp->fetch_assoc();
+	$mc = $p['mainchar'];
+	$gmc = $mysqli->query("SELECT * FROM `characters` WHERE `id`='".$mc."'") or die();
+	$m = $gmc->fetch_assoc();
 
-}elseif(@$_GET['action']=="search"){
-	if($_POST['search']){
+	require_once 'assets/libs/HTMLPurifier.standalone.php';
+	$config = HTMLPurifier_Config::createDefault();
+	$config->set('HTML.Allowed', 'p, b, u, s, ol, li, ul, i, em, strong');
+	$purifier = new HTMLPurifier($config);
+	$clean_html = $purifier->purify($p['text']);
+	if(empty($p['realname'])){
+		$p['realname'] = "";
+	} else {
+		$p['realname'] = "(" . $p['realname'] . ")";
+	}
+	echo "<h2 class=\"text-left\"><img src=\"".get_gravatar($a['email'])."\" class=\"img-circle\">&nbsp;".$name." ".htmlspecialchars($p['realname'], ENT_QUOTES, 'UTF-8')."</h2>
+	<hr/>
+	".$status." in game and ".onlineCheck(getInfo('accid', $name, 'profilename'))." on the site
+	<hr/>";
+	if(!$m['name'] == "") {
+		echo "<b>Main Character:</b> ".htmlspecialchars($m['name'], ENT_QUOTES, 'UTF-8'). "<br/>";
+	}
+	if(!empty($p['country'])) {
+		echo "<b>Country: </b>".htmlspecialchars($p['country'], ENT_QUOTES, 'UTF-8')."<br/>";
+	}
+	if(!empty($p['motto'])) {
+		echo "<b>Motto:</b> ".htmlspecialchars($p['motto'], ENT_QUOTES, 'UTF-8')."<br/>";
+	}
+	if(!empty($p['age'])) {
+		echo "<b>Age:</b> ".htmlspecialchars($p['age'], ENT_QUOTES, 'UTF-8')."<br/>";
+	}
+	if(!empty($p['favjob'])) {
+		echo "<b>Favorite Job: </b>".htmlspecialchars($p['favjob'], ENT_QUOTES, 'UTF-8')."<br/><br/>";
+	}
+	if(!empty($p['text'])) {
+		echo "
+			<b>About Me:</b>
+			<div class=\"breakword\">" . $clean_html."</div>
+			<hr/>";
+	}
+	if(isset($_SESSION['pname'])) {
+		echo "<a href=\"?base=ucp&amp;page=mail&amp;uc=$name\">Send me Mail &raquo;</a>";
+	}
+	if(isset($_SESSION['pname']) && $_GET['name'] == $_SESSION['pname']) {
+		echo "<hr/><a href=\"?base=ucp&page=profedit\">Edit Profile &raquo;</a><hr/>";
+	}
+} elseif(isset($_GET['action']) && $_GET['action'] == "search") {
+	if($_POST['search']) {
 		$name = $mysqli->real_escape_string($_POST['name']);
 		$gs = $mysqli->query("SELECT * FROM `".$prefix."profile` WHERE `name` LIKE '%".$name."%' ORDER BY `name` ASC") or die();
 		echo "
@@ -80,11 +78,10 @@ if(isset($_GET['name'])){
 			<b>Search result:</b>
 		</legend>";
 		while($s = $gs->fetch_assoc()){
-			echo "
-			<a href=\"?base=main&amp;page=members&amp;name=".$s['name']."\">".$s['name']."</a><br />";
+			echo "<a href=\"?base=main&amp;page=members&amp;name=".$s['name']."\">".$s['name']."</a><br />";
 		}
 	}
-}else{
+} else {
 	echo "
 		<h2 class=\"text-left\">Members List</h2><hr/>";
 	echo "
@@ -125,8 +122,8 @@ if(isset($_GET['name'])){
 			</tr>";
 	}
 	echo "
-	</tbody>
-</table>
+		</tbody>
+	</table>
 	";
 }
 ?>
