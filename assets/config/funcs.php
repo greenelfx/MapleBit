bea641056f90b24eea4c68ea9a0b473bbc2b6494
kedar <kedarv@users.noreply.github.com>
2016-05-24 14:37:38 -0400

    remove shortTitle for ellipsize, clean out main- files and other various files

--- assets/config/funcs.php
+++ assets/config/funcs.php
@@ -3,13 +3,22 @@ if(basename($_SERVER["PHP_SELF"]) == "funcs.php") {
 	die("403 - Access Forbidden");
 }
 
-function shortTitle($title) {
-	$maxlength = 30;
-	$title = $title." ";
-	$title = substr($title, 0, $maxlength);
-	$title = substr($title, 0, strrpos($title,' '));
-	$title = $title."...";
-	return $title;
+function ellipsize($str, $max_length, $position = 1, $ellipsis = '&hellip;') {
+	// Strip tags
+	$str = trim(strip_tags($str));
+	// Is the string long enough to ellipsize?
+	if (strlen($str) <= $max_length) {
+		return $str;
+	}
+	$beg = substr($str, 0, floor($max_length * $position));
+	$position = ($position > 1) ? 1 : $position;
+	if ($position === 1) {
+		$end = substr($str, 0, -($max_length - strlen($beg)));
+	}
+	else {
+		$end = substr($str, -($max_length - strlen($beg)));
+	}
+	return $beg.$ellipsis.$end;
 }
 
 function getRealIpAddr() {
--- sources/public/members.php
+++ sources/public/members.php
@@ -3,6 +3,11 @@ if(basename($_SERVER["PHP_SELF"]) == "members.php"){
     die("403 - Access Forbidden");
 }
 
+require_once 'assets/libs/HTMLPurifier.standalone.php';
+$config = HTMLPurifier_Config::createDefault();
+$config->set('HTML.Allowed', 'p, b, u, s, ol, li, ul, i, em, strong');
+$purifier = new HTMLPurifier($config);
+
 if(isset($_GET['name'])) {
 	$name = $mysqli->real_escape_string($_GET['name']);
 	$a = $mysqli->query("SELECT * FROM accounts INNER JOIN ".$prefix."profile ON accounts.id = ".$prefix."profile.accountid WHERE ".$prefix."profile.name = '".$name."'")->fetch_assoc();
@@ -13,27 +18,27 @@ if(isset($_GET['name'])) {
 
 	if($a['loggedin'] == "0") {
 		$status = "<span class=\"label label-danger\">Offline</span>";
-	} else {
+	}
+	else {
 		$status = "<span class=\"label label-success\">Online</span>";
 	}
+
 	$gp = $mysqli->query("SELECT * FROM `".$prefix."profile` WHERE `name`='".$name."'") or die();
 	$p = $gp->fetch_assoc();
 	$mc = $p['mainchar'];
 	$gmc = $mysqli->query("SELECT * FROM `characters` WHERE `id`='".$mc."'") or die();
 	$m = $gmc->fetch_assoc();
 
-	require_once 'assets/libs/HTMLPurifier.standalone.php';
-	$config = HTMLPurifier_Config::createDefault();
-	$config->set('HTML.Allowed', 'p, b, u, s, ol, li, ul, i, em, strong');
-	$purifier = new HTMLPurifier($config);
 	$clean_html = $purifier->purify($p['text']);
-	if(empty($p['realname'])){
+	if(empty($p['realname'])) {
 		$p['realname'] = "";
-	} else {
+	}
+	else {
 		$p['realname'] = "(" . $p['realname'] . ")";
 	}
-	echo "<h2 class=\"text-left\" style=\"display:inline-block\"><img src=\"".get_gravatar($a['email'])."\" class=\"img-circle\">&nbsp;".$name." ".htmlspecialchars($p['realname'], ENT_QUOTES, 'UTF-8') . $status . "</h2>";
-	if(!$m['name'] == "") {
+
+	echo "<h2 class=\"text-left\" style=\"display:inline-block\"><img src=\"".get_gravatar($a['email'])."\" class=\"img-circle\">&nbsp;".$name . "&nbsp;" .htmlspecialchars($p['realname'], ENT_QUOTES, 'UTF-8') . "&nbsp;" .$status . "</h2><hr/>";
+	if(!empty($m['name'])) {
 		echo "<b>Main Character:</b> ".htmlspecialchars($m['name'], ENT_QUOTES, 'UTF-8'). "<br/>";
 	}
 	if(!empty($p['country'])) {
@@ -46,32 +51,34 @@ if(isset($_GET['name'])) {
 		echo "<b>Age:</b> ".htmlspecialchars($p['age'], ENT_QUOTES, 'UTF-8')."<br/>";
 	}
 	if(!empty($p['favjob'])) {
-		echo "<b>Favorite Job: </b>".htmlspecialchars($p['favjob'], ENT_QUOTES, 'UTF-8')."<br/><br/>";
+		echo "<b>Favorite Job: </b>".htmlspecialchars($p['favjob'], ENT_QUOTES, 'UTF-8')."<br/>";
 	}
 	if(!empty($p['text'])) {
 		echo "
+			<br/>
 			<b>About Me:</b>
-			<div class=\"breakword\">" . $clean_html."</div>";
+			<div class=\"breakword\">" . $clean_html."</div>
+		";
 	}
 	if(isset($_SESSION['pname']) && $_GET['name'] == $_SESSION['pname']) {
 		echo "<hr/><a href=\"?base=ucp&page=profedit\">Edit Profile &raquo;</a><hr/>";
 	}
-} elseif(isset($_GET['action']) && $_GET['action'] == "search") {
-	if($_POST['search']) {
-		$name = $mysqli->real_escape_string($_POST['name']);
-		$gs = $mysqli->query("SELECT * FROM `".$prefix."profile` WHERE `name` LIKE '%".$name."%' ORDER BY `name` ASC") or die();
-		echo "
-		<legend>
-			<b>Search result:</b>
-		</legend>";
-		while($s = $gs->fetch_assoc()){
-			echo "<a href=\"?base=main&amp;page=members&amp;name=".$s['name']."\">".$s['name']."</a><br />";
-		}
+}
+elseif(isset($_GET['action']) && $_GET['action'] == "search" && isset($_POST['search'])) {
+	$name = $mysqli->real_escape_string($_POST['name']);
+	$gs = $mysqli->query("SELECT * FROM `".$prefix."profile` WHERE `name` LIKE '%".$name."%' ORDER BY `name` ASC") or die();
+	echo "
+		<h3 class=\"text-left\">Search Results:</h3><hr/>
+		<div class=\"list-group\">
+	";
+	while($s = $gs->fetch_assoc()){
+		echo "<a href=\"?base=main&amp;page=members&amp;name=".$s['name']."\" class=\"list-group-item\">".$s['name']."</a>";
 	}
-} else {
+	echo "</div>";
+}
+else {
 	echo "
-		<h2 class=\"text-left\">Members List</h2><hr/>";
-	echo "
+		<h2 class=\"text-left\">Members List</h2><hr/>
 		Here's the full list of the members of the <b>".$servername."</b> community.
 		You can select one to visit their profile or you can search for an user.<hr />
 		<div class=\"row\">
@@ -87,7 +94,7 @@ if(isset($_GET['name'])) {
 			</div>
 			</form>
 		</div>
-	</div><hr/>
+		</div><hr/>
 	";
 	$gp = $mysqli->query("SELECT * FROM `".$prefix."profile` WHERE `name` != 'NULL' ORDER BY `name` ASC") or die();
 	echo "<div class=\"list-group\">";
@@ -96,4 +103,3 @@ if(isset($_GET['name'])) {
 	}
 	echo "</div>";
 }
-?>
