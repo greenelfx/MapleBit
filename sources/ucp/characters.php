ac9cba256eefc6c18ed035cabecbf02910fd9f85
Kedar <kedar54@gmail.com>
2016-05-26 11:27:42 -0400

    minor cleanups, make account settings more robust

--- sources/ucp/characters.php
+++ sources/ucp/characters.php
@@ -3,13 +3,6 @@ if(basename($_SERVER["PHP_SELF"]) == "characters.php"){
 	die("403 - Access Forbidden");
 }
 if(isset($_SESSION['id'])){
-	if($servertype == 1) {
-		$first = "reborns";
-		$second = "level";
-	} else {
-		$first = "level";
-		$second = "exp";
-	}
 	$checkchar = $mysqli->query("SELECT * from characters WHERE accountid = '".$_SESSION['id']."'");
 	$countchar = $checkchar->num_rows;
 	if($countchar > 0) {
@@ -20,20 +13,23 @@ if(isset($_SESSION['id'])){
 				echo "<div class=\"row\">";
 			}
 			echo "
-			<div class=\"col-md-4\">
-				<div class=\"well\">
-					<h3 class=\"text-center\"> " . $c['name'] . "</h3>
-					<hr/>
-					<img src=\"assets/img/GD/create.php?name=".$c['name']."\" alt='".$c['name']."' class=\"avatar img-responsive\" style=\"margin: 0 auto;\">
-					<hr/>
-					<b>Job:</b> " . $c['job'] . "<br/>";
-					if($servertype == 1) {
-						echo "<b>Rebirths:</b> " . $c['reborns'] . "<br/>";
-					}
-					echo "	<b>Level:</b> " . $c['level'] . "<br/>
-					<b>EXP:</b> " . $c['exp'] . "<br/>
+				<div class=\"col-md-4\">
+					<div class=\"well\">
+						<h3 class=\"text-center\"> " . $c['name'] . "</h3>
+						<hr/>
+						<img src=\"assets/img/GD/create.php?name=".$c['name']."\" alt='".$c['name']."' class=\"avatar img-responsive\" style=\"margin: 0 auto;\">
+						<hr/>
+						<b>Job:</b> " . $c['job'] . "<br/>
+			";
+			if($servertype == 1) {
+				echo "<b>Rebirths:</b> " . $c['reborns'] . "<br/>";
+			}
+			echo "
+						<b>Level:</b> " . $c['level'] . "<br/>
+						<b>EXP:</b> " . $c['exp'] . "<br/>
+					</div>
 				</div>
-			</div>";
+			";
 			$i++;
 			if($i % 3 == 0) {
 				echo "</div>";
@@ -46,4 +42,3 @@ if(isset($_SESSION['id'])){
 } else {
 	redirect("?base=main");
 }
-?>
