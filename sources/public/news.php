1d37a153ead4541e876145edd58750d6659971ae
Kedar <kedar54@gmail.com>
2016-07-13 20:23:03 -0400

    fix chrome duplicate load, cleanup admin background tool

--- sources/public/news.php
+++ sources/public/news.php
@@ -10,11 +10,13 @@ if(basename($_SERVER["PHP_SELF"]) == "news.php"){
 }
 </style>
 <?php
-if(isset($_GET['id'])){
+if(isset($_GET['id'])) {
 	$id = $mysqli->real_escape_string($_GET['id']);
-	$gn = $mysqli->query("SELECT * FROM ".$prefix."news WHERE id='".$id."'") or die();
+	$gn = $mysqli->query("SELECT * FROM ".$prefix."news WHERE id='".$id."'");
 	$n = $gn->fetch_assoc();
-	require_once 'assets/libs/HTMLPurifier.standalone.php';
+	if($gn->num_rows) {
+		$mysqli->query("UPDATE `".$prefix."news` SET `views` = views + 1 WHERE `id`='".$id."'");
+		require_once 'assets/libs/HTMLPurifier.standalone.php';
 		$config = HTMLPurifier_Config::createDefault();
 		$config->set('HTML.SafeIframe', true);
 		$config->set('HTML.TargetBlank', true);
@@ -46,147 +48,155 @@ if(isset($_GET['id'])){
 			$negative = ($negative/$cc)*100;
 			$neutral = ($neutral/$cc)*100;
 		}
-	echo "
-		<h2 class=\"text-left\">".htmlspecialchars($n['title'], ENT_QUOTES, 'UTF-8')." | Posted by <a href=\"?base=main&amp;page=members&amp;name=".$n['author']."\">".$n['author']."</a> on ".$n['date']."</h2><hr/>
+		echo "
+			<h2 class=\"text-left\">".htmlspecialchars($n['title'], ENT_QUOTES, 'UTF-8')." | Posted by <a href=\"?base=main&amp;page=members&amp;name=".$n['author']."\">".$n['author']."</a> on ".	$n['date']."</h2><hr/>
+			";
+		echo "<div class=\"breakword\">" . $clean_html."</div><hr/>";
+		echo "
+			<b>".$n['views']."</b> Views and <b>".$cc."</b> Responses<hr />
+			<div class=\"progress\">
+				<div class=\"progress-bar progress-bar-success\" style=\"width: ".$positive."%\">
+					<span class=\"sr-only\">".$positive."% (positive)</span>
+				</div>
+				<div class=\"progress-bar progress-bar-danger\" style=\"width: ".$negative."%\">
+					<span class=\"sr-only\">".$negative."% (negative)</span>
+				</div>
+				<div class=\"progress-bar progress-bar-default\" style=\"width: ".$neutral."%\">
+					<span class=\"sr-only\">".$neutral." (neutral)</span>
+				</div>
+			</div>
 		";
-	echo "<div class=\"breakword\">" . $clean_html."</div><hr/>";
-	echo "
-	<b>".$n['views']."</b> Views and <b>".$cc."</b> Responses<hr />
-	<div class=\"progress\">
-		<div class=\"progress-bar progress-bar-success\" style=\"width: ".$positive."%\">
-			<span class=\"sr-only\">".$positive."% (positive)</span>
-		</div>
-		<div class=\"progress-bar progress-bar-danger\" style=\"width: ".$negative."%\">
-			<span class=\"sr-only\">".$negative."% (negative)</span>
-		</div>
-		<div class=\"progress-bar progress-bar-default\" style=\"width: ".$neutral."%\">
-			<span class=\"sr-only\">".$neutral." (neutral)</span>
-		</div>
-	</div>";
-	$av = $mysqli->query("UPDATE `".$prefix."news` SET `views` = views + 1 WHERE `id`='".$id."'") or die();
-	if(isset($_SESSION['admin'])){
-		if($n['locked'] == "1"){
-			$buttontext = "Unlock";
-			$buttonlink = "unlock";
-		}
-		else {$buttontext = "Lock"; $buttonlink = "lock";}
-		echo "
-			<a href=\"?base=admin&amp;page=mannews&amp;action=edit&amp;id=".$n['id']."\" class=\"btn btn-primary\">Edit</a>
-			<a href=\"?base=admin&amp;page=mannews&amp;action=del\" class=\"btn btn-info\">Delete</a>
-			<a href=\"?base=admin&amp;page=mannews&amp;action=".$buttonlink."\" class=\"btn btn-default\">".$buttontext."</a>
-			<hr />";
-	}
-	if(isset($_SESSION['id'])){
-	$flood = $mysqli->query("SELECT * FROM `".$prefix."ncomments` WHERE `nid`='".$id."' && `author`='".$_SESSION['pname']."' ORDER BY `date` DESC LIMIT 1") or die();
-	$fetchg = $flood->fetch_assoc();
-	$seconds = 60*$basefloodint;
-	$editor = false;
-		if($_SESSION['mute'] == 1) {
-			echo "<div class=\"alert alert-danger\">You have been muted. Please contact an administrator</div>";
-		}
-		elseif($n['locked'] == "1") {
-			echo "<div class=\"alert alert-danger\">This article has been locked.</div>";
-		} elseif($_SESSION['pname'] == "checkpname"){
-			echo "<div class=\"alert alert-danger\">You must assign a profile name before you can comment news articles.</div>";
-		} elseif($baseflood > 0 && (time() - $seconds) < $fetchg['date']) {
-			echo "<div class=\"alert alert-danger\">You may only post every ".$basefloodint." minutes to prevent spam.</div>";
-		} else {
-			$editor = true;
+		if(isset($_SESSION['admin'])) {
+			if($n['locked'] == "1") {
+				$buttontext = "Unlock";
+				$buttonlink = "unlock";
+			}
+			else {$buttontext = "Lock"; $buttonlink = "lock";}
 			echo "
-			<form method=\"post\" id=\"commentBox\">
-				 <div class=\"form-group\">
-					<label for=\"inputMood\">Mood</label>
-						<select name=\"feedback\" class=\"form-control\" id=\"inputMood\">
-							<option value=\"0\">Positive</option>
-							<option value=\"1\">Neutral</option>
-							<option value=\"2\">Negative</option>
-						</select>
-					</div>
-					<div class=\"form-group\">
-						<label for=\"inputComment\">Comment:</label>
-						<textarea name=\"text\" class=\"form-control\" rows=\"5\" id=\"inputComment\"></textarea>
-					</div>
-					<hr/>
-					<input type=\"submit\" name=\"comment\" value=\"Comment\" class=\"btn btn-primary\"/>
-			</form>";
+				<a href=\"?base=admin&amp;page=mannews&amp;action=edit&amp;id=".$n['id']."\" class=\"btn btn-primary\">Edit</a>
+				<a href=\"?base=admin&amp;page=mannews&amp;action=del\" class=\"btn btn-info\">Delete</a>
+				<a href=\"?base=admin&amp;page=mannews&amp;action=".$buttonlink."\" class=\"btn btn-default\">".$buttontext."</a>
+				<hr />";
 		}
-	}else{
-		echo "
-			<div class=\"alert alert-danger\">Please log in to comment.</div>";
-	}
-	if(isset($_POST['comment'])){
-		$feedback = $mysqli->real_escape_string($_POST['feedback']);
-		$comment = $mysqli->real_escape_string($_POST['text']);
-		if($comment == ""){
-			echo "<br/><div class=\"alert alert-danger\">You cannot leave the comment field blank!</div>";
-		}else{
-			$date = time();
-			$i = $mysqli->query("INSERT INTO ".$prefix."ncomments (nid, author, feedback, date, comment) VALUES ('".$id."','".$_SESSION['pname']."','".$feedback."','".$date."','".$comment."')") or die();
-			echo "<meta http-equiv=refresh content=\"0; url=?base=main&amp;page=news&amp;id=".$id."\" />";
+		if(isset($_SESSION['id'])) {
+			$flood = $mysqli->query("SELECT * FROM `".$prefix."ncomments` WHERE `nid`='".$id."' && `author`='".$_SESSION['pname']."' ORDER BY `date` DESC LIMIT 1") or die();
+			$fetchg = $flood->fetch_assoc();
+			$seconds = 60*$basefloodint;
+			$editor = false;
+			if($_SESSION['mute'] == 1) {
+				echo "<div class=\"alert alert-danger\">You have been muted. Please contact an administrator</div>";
+			}
+			elseif($n['locked'] == "1") {
+				echo "<div class=\"alert alert-danger\">This article has been locked.</div>";
+			} elseif($_SESSION['pname'] == "checkpname") {
+				echo "<div class=\"alert alert-danger\">You must assign a profile name before you can comment news articles.</div>";
+			} elseif($baseflood > 0 && (time() - $seconds) < $fetchg['date']) {
+				echo "<div class=\"alert alert-danger\">You may only post every ".$basefloodint." minutes to prevent spam.</div>";
+			} else {
+				$editor = true;
+				echo "
+				<form method=\"post\" id=\"commentBox\">
+					 <div class=\"form-group\">
+						<label for=\"inputMood\">Mood</label>
+							<select name=\"feedback\" class=\"form-control\" id=\"inputMood\">
+								<option value=\"0\">Positive</option>
+								<option value=\"1\">Neutral</option>
+								<option value=\"2\">Negative</option>
+							</select>
+						</div>
+						<div class=\"form-group\">
+							<label for=\"inputComment\">Comment:</label>
+							<textarea name=\"text\" class=\"form-control\" rows=\"5\" id=\"inputComment\"></textarea>
+						</div>
+						<hr/>
+						<input type=\"submit\" name=\"comment\" value=\"Comment\" class=\"btn btn-primary\"/>
+				</form>";
+			}
 		}
-	}
-	echo "<hr />";
-
-	if($cgc = $gc->num_rows <= 0 && $n['locked'] == 0){
-		echo "<div class=\"alert alert-info\">There are no comments for this article yet. Be the first to comment!</div>";
-	}else{
-		$commentconfig = HTMLPurifier_Config::createDefault();
-		$commentconfig->set('HTML.Allowed', 'p, b, u, s, ol, li, ul, i, em, strong, blockquote, small, hr');
-		$commentpurifier = new HTMLPurifier($commentconfig);
-		while($c = $gc->fetch_assoc()){
-		$clean_comment = $commentpurifier->purify($c['comment']);
-			if($c['feedback'] == "0"){
-				$feedback = "<span class=\"positive_comment\">Positive</span>";
-			}elseif($c['feedback'] == "1"){
-				$feedback = "<span class=\"neutral_comment\">Neutral</span>";
-			}elseif($c['feedback'] == "2"){
-				$feedback = "<span class=\"negative_comment\">Negative</span>";
-			}
-			$modify = "";
-			if(isset($_SESSION['admin'])){
-				$modify = "<a href=\"?base=admin&amp;page=mannews&amp;action=pdel&amp;id=".$c['id']."\">Delete</a> | ";
-			}
-			$quote = "";
-			if(isset($_SESSION['id'])){
-				$quote = "<a href=\"#comment-".$c['id']."-".$c['author']."\" class=\"quote\">Quote</a> | ";
-			}
+		else{
 			echo "
-			<div class=\"well\"><img src=\"" . get_gravatar($c['email']) . "\" alt=\"".$c['author']."\" class=\"img-responsive\" style=\"float:left;padding-right:10px;\"/>
-			<h4 style=\"margin:0px;\">".$c['author']."</h4>
-				<b>Feedback:</b> ".$feedback."<br/>
-				<small>Posted ".ago($c['date']).", on ". date('M j, Y', $c['date'])."</small><br/>
-				<small>".$modify . $quote."<a href=\"#comment-link-".$c['id']."\" class=\"permalink\">Permalink</a><a href=\"?base=main&page=news&id=".$id."#comment-".$c['id']."\" class=\"permalinkshow linkid-".$c['id']."\">?base=main&page=news&id=".$id."#comment-".$c['id']."</a></small><hr/>
-				<div class=\"breakword\" id=\"comment-".$c['id']."\">".$clean_comment."</div>
-				</div>";
+				<div class=\"alert alert-danger\">Please log in to comment.</div>";
+		}
+		if(isset($_POST['comment'])){
+			$feedback = $mysqli->real_escape_string($_POST['feedback']);
+			$comment = $mysqli->real_escape_string($_POST['text']);
+			if($comment == ""){
+				echo "<br/><div class=\"alert alert-danger\">You cannot leave the comment field blank!</div>";
+			}
+			else {
+				$date = time();
+				$i = $mysqli->query("INSERT INTO ".$prefix."ncomments (nid, author, feedback, date, comment) VALUES ('".$id."','".$_SESSION['pname']."','".$feedback."','".$date."','".$comment."')	") or die();
+				echo "<meta http-equiv=refresh content=\"0; url=?base=main&amp;page=news&amp;id=".$id."\" />";
+			}
+		}
+		echo "<hr />";
+	
+		if($cgc = $gc->num_rows <= 0 && $n['locked'] == 0) {
+			echo "<div class=\"alert alert-info\">There are no comments for this article yet. Be the first to comment!</div>";
+		}
+		else {
+			$commentconfig = HTMLPurifier_Config::createDefault();
+			$commentconfig->set('HTML.Allowed', 'p, b, u, s, ol, li, ul, i, em, strong, blockquote, small, hr');
+			$commentpurifier = new HTMLPurifier($commentconfig);
+			while($c = $gc->fetch_assoc()){
+			$clean_comment = $commentpurifier->purify($c['comment']);
+				if($c['feedback'] == "0"){
+					$feedback = "<span class=\"positive_comment\">Positive</span>";
+				}elseif($c['feedback'] == "1"){
+					$feedback = "<span class=\"neutral_comment\">Neutral</span>";
+				}elseif($c['feedback'] == "2"){
+					$feedback = "<span class=\"negative_comment\">Negative</span>";
+				}
+				$modify = "";
+				if(isset($_SESSION['admin'])){
+					$modify = "<a href=\"?base=admin&amp;page=mannews&amp;action=pdel&amp;id=".$c['id']."\">Delete</a> | ";
+				}
+				$quote = "";
+				if(isset($_SESSION['id'])){
+					$quote = "<a href=\"#comment-".$c['id']."-".$c['author']."\" class=\"quote\">Quote</a> | ";
+				}
+				echo "
+				<div class=\"well\"><img src=\"" . get_gravatar($c['email']) . "\" alt=\"".$c['author']."\" class=\"img-responsive\" style=\"float:left;padding-right:10px;\"/>
+				<h4 style=\"margin:0px;\">".$c['author']."</h4>
+					<b>Feedback:</b> ".$feedback."<br/>
+					<small>Posted ".ago($c['date']).", on ". date('M j, Y', $c['date'])."</small><br/>
+					<small>".$modify . $quote."<a href=\"#comment-link-".$c['id']."\" class=\"permalink\">Permalink</a><a href=\"?base=main&page=news&id=".$id."#comment-".$c['id']."\" class=\"	permalinkshow linkid-".$c['id']."\">?base=main&page=news&id=".$id."#comment-".$c['id']."</a></small><hr/>
+					<div class=\"breakword\" id=\"comment-".$c['id']."\">".$clean_comment."</div>
+					</div>";
+			}
 		}
 	}
-}else{
+	else {
+		echo "invalid id";
+	}
+}
+else {
 	$gn = $mysqli->query("SELECT * FROM ".$prefix."news ORDER BY id DESC") or die();
 	$rows = $gn->num_rows;
 	if ($rows < 1) {
 		echo "<div class=\"alert alert-danger\">Oops! There isn't any news to display right now!</div>";
 	}
-	else{
-	echo "<h2 class=\"text-left\">".$servername." News</h2><hr/>";
-	while($n = $gn->fetch_assoc()){
-		$gc = $mysqli->query("SELECT * FROM ".$prefix."ncomments WHERE nid='".$n['id']."' ORDER BY id ASC") or die();
-		$cc = $gc->num_rows;
-		echo "
-			<img src=\"assets/img/news/".$n['type'].".gif\" alt='".$n['type']."' />
-			[".$n['date']."] <b><a href=\"?base=main&page=news&amp;id=".$n['id']."\">".htmlspecialchars($n['title'], ENT_QUOTES, 'UTF-8')."</a></b>
-		<span class=\"commentbubble\">
-			<b>".$n['views']."</b> views | <b>".$cc."</b> comments
-		";
-		if(isset($_SESSION['admin'])){
+	else {
+		echo "<h2 class=\"text-left\">".$servername." News</h2><hr/>";
+		while($n = $gn->fetch_assoc()) {
+			$gc = $mysqli->query("SELECT * FROM ".$prefix."ncomments WHERE nid='".$n['id']."' ORDER BY id ASC") or die();
+			$cc = $gc->num_rows;
 			echo "
-				<a href=\"?base=admin&amp;page=mannews&amp;action=edit&amp;id=".$n['id']."\">Edit</a> |
-				<a href=\"?base=admin&amp;page=mannews&amp;action=del\">Delete</a> |
-				<a href=\"?base=admin&amp;page=mannews&amp;action=lock\">Lock</a>&nbsp;
+				<img src=\"assets/img/news/".$n['type'].".gif\" alt='".$n['type']."' />
+				[".$n['date']."] <b><a href=\"?base=main&page=news&amp;id=".$n['id']."\">".htmlspecialchars($n['title'], ENT_QUOTES, 'UTF-8')."</a></b>
+			<span class=\"commentbubble\">
+				<b>".$n['views']."</b> views | <b>".$cc."</b> comments
 			";
+			if(isset($_SESSION['admin'])) {
+				echo "
+					<a href=\"?base=admin&amp;page=mannews&amp;action=edit&amp;id=".$n['id']."\">Edit</a> |
+					<a href=\"?base=admin&amp;page=mannews&amp;action=del\">Delete</a> |
+					<a href=\"?base=admin&amp;page=mannews&amp;action=lock\">Lock</a>&nbsp;
+				";
+			}
+			echo "</span><br/>";
 		}
-		echo "</span><br/>";
 	}
-}
 }
 ?>
 <script>
