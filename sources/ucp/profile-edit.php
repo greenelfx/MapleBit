41dcffd03d0b14cfa83eb03825184a61f882caa2
greenelfx <7076163+greenelfx@users.noreply.github.com>
2016-08-14 09:42:32 -0500

    cleanup profile actions

--- sources/ucp/profile-edit.php
+++ sources/ucp/profile-edit.php
@@ -7,160 +7,137 @@ if(basename($_SERVER["PHP_SELF"]) == "profile-edit.php") {
 <?php
 if($_SESSION['pname'] === "checkpname") {
 	echo "<div class=\"alert alert-danger\">You must assign a profile name before you can edit your public profile.</div>";
+	return;
 }
-else {
-	echo "<h2 class=\"text-left\">My Profile</h2><hr/>";
-	if(!isset($_POST['edit'])) {
-		$gp = $mysqli->query("SELECT * FROM ".$prefix."profile WHERE accountid='".$_SESSION['id']."'") or die();
-		$p = $gp->fetch_assoc();
-		$gc = $mysqli->query("SELECT * FROM characters WHERE accountid='".$_SESSION['id']."'") or die();
-		$cgc = $gc->num_rows;
-		echo "
-			<form method=\"post\" role=\"form\">
-			<b>Profile Name: </b>".$p['name']."
-			<div class=\"form-group\">
-		";
-		if($cgc > 0) {
-			echo "
-				<label for=\"mainChar\">Main Character:</label>
-				<select name=\"mainchar\" class=\"form-control\" id=\"mainChar\">
-			";
-			while($c = $gc->fetch_assoc()) {
-				echo "<option value=\"".$c['id']."\">".$c['name']."</option>";
-			}
-			echo "</select>";
+
+require "assets/libs/gump.class.php";
+GUMP::add_validator("exists", function($field, $input, $param = NULL) use ($mysqli, $prefix) {
+	return $mysqli->query("SELECT COUNT(*) FROM characters WHERE $param ='".$mysqli->real_escape_string($input[$field])."'")->fetch_row()[0] == 0;
+});
+if(isset($_POST['edit'])) {
+	$gump = new GUMP();
+	$_POST = $gump->sanitize($_POST);
+	$gump->validation_rules(array(
+		'mainchar' => 'exists,name',
+		'realname' => 'alpha_space',
+		'age' => 'numeric|min_numeric,7|max_numeric,50',
+		'country' => 'alpha_space',
+		'motto' => 'alpha_space',
+		'text' => 'max_len, 200'
+	));
+	$gump->filter_rules(array(
+	    'mainchar' => 'trim|sanitize_string',
+	    'realname' => 'trim|sanitize_string',
+	    'age' => 'trim|sanitize_string',
+	    'country' => 'trim|sanitize_string',
+	    'motto' => 'trim|sanitize_string',
+	    'favjob' => 'trim|sanitize_string',
+	    'text' => 'sanitize_string',
+	));
+	$validated_data = $gump->run($_POST);
+	if($validated_data === false) {
+		echo '<div class="alert alert-danger">';
+		foreach($gump->get_errors_array() as $error) {
+			echo $error . '<br/>';
 		}
-		else {
-			echo "<hr/><div class=\"alert alert-danger\">You don't have any characters!</div><hr/>";
-		}
-		echo "
-			</div>
-			<div class=\"form-group\">
-				<label for=\"realName\">Real Name:</label>
-				<input type=\"text\" class=\"form-control\" name=\"realname\" value=\"".htmlspecialchars($p['realname'], ENT_QUOTES, 'UTF-8')."\" id=\"realName\"/>
-			</div>
-			<div class=\"form-group\">
-				<label for=\"myAge\">Age: </label>
-				<select name=\"age\" class=\"form-control\" id=\"myAge\">
-					<option value=\"".$p['age']."\">".$p['age']."</option>
-		";
-		$i = 7;
-		while($i < 50) {
-			echo "<option value=\"".$i."\">".$i."</option>";
-			$i++;
-		}
-		echo "
-				</select>
-			</div>
-			<div class=\"form-group\">
-			<label for=\"Country\">Country:</label>
-				<select id=\"Country\" name=\"country\" class=\"form-control\">
-		";
-		$countries = getCountries();
-		foreach($countries as $country) {
-			echo "<option value=\"".$country."\">".$country."</option>";
-		}
-		echo "
-				</select>
-			</div>
-			<div class=\"form-group\">
-				<label for=\"Motto\">Motto:</label>
-				<input type=\"text\" class=\"form-control\" name=\"motto\" value=\"".htmlspecialchars($p['motto'], ENT_QUOTES, 'UTF-8')."\" id=\"Motto\"/>
-			</div>
-			<div class=\"form-group\">
-				<label for=\"favJob\">Favorite Job:</label>
-					<select name=\"favjob\" class=\"form-control\" id=\"favJob\">
-		";
-		if(isset($p['favjob'])) {
-			echo "<option value=\"".$p['favjob']."\">".$p['favjob']."</option>";
-		}
-		echo "
-			<optgroup label=\"Beginner\">
-				<option value=\"Beginner\">Beginner</option>
-				<option value=\"PermaNoob\">PermaNoob</option>
-			</optgroup>
-			<optgroup label=\"Warrior\">
-				<option value=\"Swordman\">Swordman</option>
-				<option value=\"Fighter\">Fighter</option>
-				<option value=\"Spearman\">Spearman</option>
-				<option value=\"Page\">Page</option>
-				<option value=\"Crusader\">Crusader</option>
-				<option value=\"Dragon Knight\">Dragon Knight</option>
-				<option value=\"White Knight\">White Knight</option>
-				<option value=\"Hero\">Hero</option>
-				<option value=\"Dark Knight\">Dark Knight</option>
-				<option value=\"Paladin\">Paladin</option>
-			</optgroup>
-			<optgroup label=\"Bowman\">
-				<option value=\"Archer\">Archer</option>
-				<option value=\"Hunter\">Hunter</option>
-				<option value=\"Crossbowman\">Crossbowman</option>
-				<option value=\"Ranger\">Ranger</option>
-				<option value=\"Sniper\">Sniper</option>
-				<option value=\"Bowmaster\">Bowmaster</option>
-				<option value=\"Marksman\">Marksman</option>
-			</optgroup>
-			<optgroup label=\"Magician\">
-				<option value=\"Magician\">Magician</option>
-				<option value=\"I/L Wizard\">I/L Wizard</option>
-				<option value=\"F/P Wizard\">F/P Wizard</option>
-				<option value=\"Cleric\">Cleric</option>
-				<option value=\"I/L Mage\">I/L Mage</option>
-				<option value=\"F/P Mage\">F/P Mage</option>
-				<option value=\"Priest\">Priest</option>
-				<option value=\"I/L Arch Mage\">I/L Arch Mage</option>
-				<option value=\"F/P Arch Mage\">F/P Arch Mage</option>
-				<option value=\"Bishop\">Bishop</option>
-				</optgroup>
-			<optgroup label=\"Theif\">
-				<option value=\"Rogue\">Rogue</option>
-				<option value=\"Assassin\">Assassin</option>
-				<option value=\"Bandit\">Bandit</option>
-				<option value=\"Hermit\">Hermit</option>
-				<option value=\"Chief Bandit\">Chief Bandit</option>
-				<option value=\"Night Lord\">Night Lord</option>
-				<option value=\"Shadower\">Shadower</option>
-			</optgroup>
-			<optgroup label=\"Pirate\">
-				<option value=\"Pirate\">Pirate</option>
-				<option value=\"Infighter\">Infighter</option>
-				<option value=\"Gunslinger\">Gunslinger</option>
-				<option value=\"Valkyrie\">Valkyrie</option>
-				<option value=\"Buccaneer\">Buccaneer</option>
-				<option value=\"Viper\">Viper</option>
-				<option value=\"Captain\">Captain</option>
-			</optgroup>
-			</select>
-			</div>
-			<div class=\"form-group\">
-				<label>About Me:</label>
-				<textarea name=\"text\" style=\"height:200px\" maxlength=\"200\" class=\"form-control\" id=\"textCount\">".stripslashes($p['text'])."</textarea>
-			</div>
-			<p id=\"counter\">Characters left: 200</p>
-			<div class=\"alert alert-info\">Please keep in mind that all of this information will be public.</div>
-			<input type=\"submit\" name=\"edit\" value=\"Update &raquo;\" class=\"btn btn-primary\"/>
-			</form>
-		";
-	}
-	else {
-		$pname = $mysqli->real_escape_string(isset($_POST['pname']));
-		if(isset($_POST['mainchar'])) {
-			$mainchar = $mysqli->real_escape_string($_POST['mainchar']);
-		} else {
-			$mainchar = "";
-		}
-		$realname = $mysqli->real_escape_string($_POST['realname']);
-		$age = $mysqli->real_escape_string($_POST['age']);
-		$country = $mysqli->real_escape_string($_POST['country']);
-		$motto = $mysqli->real_escape_string($_POST['motto']);
-		$favjob = $_POST['favjob'];
-		$text = $mysqli->real_escape_string($_POST['text']);
-		$u = $mysqli->query("UPDATE `".$prefix."profile` SET `mainchar`='".$mainchar."',`realname`='".$realname."',`age`='".$age."',`country`='".$country."',`motto`='".$motto."',`favjob`='".$favjob."',`text`='".$text."' WHERE `accountid`='".$_SESSION['id']."'") or die(mysql_error());
+		echo '</div>';
+	} else {
+ 		$u = $mysqli->query("UPDATE `".$prefix."profile` SET `mainchar`='".$validated_data['mainchar']."',`realname`='".$validated_data['realname']."',`age`='".$validated_data['age']."',`country`='".$validated_data['country']."',`motto`='".$validated_data['motto']."',`favjob`='".$mysqli->real_escape_string($validated_data['favjob'])."',`text`='".$mysqli->real_escape_string($validated_data['text'])."' WHERE `accountid`='".$_SESSION['id']."'");
 		echo "<div class=\"alert alert-success\">Your public profile has been updated<br />";
 		echo "Click <a href=\"?base=main&amp;page=members&name=".$_SESSION['pname']."\" class=\"alert-link\">here</a> to go to your profile.</div>";
 	}
 }
-
+else {
+	$profile =  $mysqli->query("SELECT * FROM ".$prefix."profile WHERE accountid='".$_SESSION['id']."'")->fetch_assoc();
+	$getCharacters = $mysqli->query("SELECT * FROM characters WHERE accountid='".$_SESSION['id']."'");
+?>
+	<h2 class="text-left">My Profile</h2><hr/>
+	<form method="post" role="form">
+		<b>Profile Name: </b> <?php echo $profile['name'] ?>
+		<div class="form-group">
+		<?php
+			if($getCharacters->num_rows) {
+				echo "
+					<label for=\"mainChar\">Main Character:</label>
+					<select name=\"mainchar\" class=\"form-control\" id=\"mainChar\">
+				";
+				while($c = $getCharacters->fetch_assoc()) {
+					echo "<option value=\"".$c['id']."\">".$c['name']."</option>";
+				}
+				echo "</select>";
+			} else {
+				echo "<hr/><div class=\"alert alert-danger\">You don't have any characters!</div><hr/>";
+			}
+		?>
+		</div>
+		<div class="form-group">
+			<label for="realName">Real Name:</label>
+			<input type="text" class="form-control" name="realname" id="realName" value="<?php echo htmlspecialchars($profile['realname'], ENT_QUOTES, 'UTF-8') ?>">
+		</div>
+		<div class="form-group">
+			<label for="myAge">Age: </label>
+			<select name="age" class="form-control" id="myAge">
+				<?php
+				if(!isset($profile['age'])) {
+					echo "<option disabled selected>Select Age</option>";
+				}
+				for($i = 7; $i < 50; $i++) {
+					if(isset($profile['age']) && $i == $profile['age']) {
+						echo "<option value=\"".$i."\" selected>".$i."</option>";
+					} else {
+						echo "<option value=\"".$i."\">".$i."</option>";
+					}
+				}
+				?>
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="inputCountry">Country:</label>
+			<select id="inputCountry" name="country" class="form-control">
+			<?php
+				if(!isset($profile['country'])) {
+					echo "<option disabled selected>Select Country</option>";
+				}
+				$countries = getCountries();
+				foreach($countries as $country) {
+					if(isset($profile['country']) && $country == $profile['country']) {
+						echo "<option value=\"".$country."\" selected>".$country."</option>";
+					} else {
+						echo "<option value=\"".$country."\">".$country."</option>";
+					}
+				}
+			?>
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="inputMotto">Motto:</label>
+			<input type="text" class="form-control" name="motto" id="inputMotto" value="<?php echo htmlspecialchars($profile['motto'], ENT_QUOTES, 'UTF-8') ?>">
+		</div>
+		<div class="form-group">
+			<label for="favJob">Favorite Job:</label>
+			<select name="favjob" class="form-control" id="favJob">
+			<?php
+				$jobs = getJobNames(true);
+				foreach($jobs as $job) {
+					if(isset($profile['favjob']) && $job == $profile['favjob']) {
+						echo "<option value=\"".$job."\" selected>".$job."</option>";
+					} else {
+						echo "<option value=\"".$job."\">".$job."</option>";
+					}
+				}
+			?>
+			</select>
+		</div>
+		<div class="form-group">
+			<label>About Me:</label>
+			<textarea name="text" style="height:200px" maxlength="200" class="form-control" id="textCount"><?php echo stripslashes($profile['text']) ?></textarea>
+		</div>
+		<p id="counter">Characters left: 200</p>
+		<div class="alert alert-info">Please keep in mind that all of this information will be public.</div>
+		<input type="submit" name="edit" value="Update &raquo;" class="btn btn-primary"/>
+	</form>
+<?php
+}
 ?>
 <script>
 	CKEDITOR.replace('textCount');
