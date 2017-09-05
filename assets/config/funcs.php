055d28c299fa5d0b5ba4a8dde90950928d1eb87c
Brandon Stewart <bstewart@northern.co>
2017-09-05 09:09:44 -0400

    Added https to gravatar

--- assets/config/funcs.php
+++ assets/config/funcs.php
@@ -57,7 +57,7 @@ function redirect_wait5($url) {
 }
 
 function get_gravatar($email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array()) {
-    $url = 'http://www.gravatar.com/avatar/';
+    $url = 'https://www.gravatar.com/avatar/';
     $url .= md5( strtolower( trim( $email ) ) );
     $url .= "?s=$s&amp;d=identicon&amp;r=$r";
     if($img) {
